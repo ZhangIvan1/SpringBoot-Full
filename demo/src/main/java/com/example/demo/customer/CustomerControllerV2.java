23968c1a8d14d7b7f8231ff8bc5a449d2fdacf5b
ZhangIvan1 <zhang_ivan1@163.com>
2024-01-31 13:33:01 +0000

    improve api

--- demo/src/main/java/com/example/demo/customer/CustomerControllerV2.java
+++ demo/src/main/java/com/example/demo/customer/CustomerControllerV2.java
@@ -4,21 +4,26 @@ package com.example.demo.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
-@RequestMapping(path = "api/v1/customer")
+@RequestMapping(path = "api/v2/customer")
 @RestController
-@Deprecated
-public class CustomerController {
+public class CustomerControllerV2 {
     private final CustomerService customerService;
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerControllerV2(CustomerService customerService) {
         this.customerService = customerService;
     }
     @GetMapping(value = "all")
-    List<Customer> getCustomer() {
+    List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @GetMapping(path = "{customerId}")
+    Customer getCustomer(@PathVariable("customerId") Long id){
+        return customerService.getCustomer(id);
     }
 
     @PostMapping
