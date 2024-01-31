f8bf00235004c1928ac8bd571dcc3384ff2e9dc6
ZhangIvan1 <zhang_ivan1@163.com>
2024-01-31 10:17:59 +0000

    @Bean

---
+++ demo/src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.demo.customer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomerConfiguration {
+    @Value("${app.userFakeCustomerRepo:false}")
+    private Boolean userFakeCustomerRepo;
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("Command line runner hooray");
+        };
+    }
+
+    @Bean
+    CustomerRepo customerRepo() {
+        System.out.println("userFakeCustomerRepo = " + userFakeCustomerRepo);
+        return userFakeCustomerRepo?
+                new CustomerFakeRepository() :
+                new CustomerRepository();
+    }
+}
--- demo/src/main/java/com/example/demo/customer/CustomerRepository.java
+++ demo/src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -2,16 +2,22 @@ package com.example.demo.customer;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@Component
-@Primary
+//@Repository
+//@Primary
 public class CustomerRepository implements CustomerRepo{
+
     @Override
     public List<Customer> getCustomers() {
         // TODO connect to real db
-        return Collections.emptyList();
+        return Arrays.asList(
+                new Customer(1L,"Ivan Zhang"),
+                new Customer(2L,"Jamila Ahmed")
+        );
     }
 }
