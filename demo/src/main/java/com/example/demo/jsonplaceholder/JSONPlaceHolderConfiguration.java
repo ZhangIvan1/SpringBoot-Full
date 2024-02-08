79c540d3d59c93a16469bbf4a3294a246339f991
ZhangIvan1 <zhang_ivan1@163.com>
2024-02-08 08:22:11 +0000

    Spring Profiles

--- demo/src/main/java/com/example/demo/jsonplaceholder/JSONPlaceHolderConfiguration.java
+++ demo/src/main/java/com/example/demo/jsonplaceholder/JSONPlaceHolderConfiguration.java
@@ -8,14 +8,13 @@ import org.springframework.context.annotation.Configuration;
 public class JSONPlaceHolderConfiguration {
     @Bean("jsonplaceholder")
     CommandLineRunner runner(JSONPlaceHolderClient
-                             jsonPlaceHolderClient) {
+                                     jsonPlaceHolderClient) {
         return args -> {
             System.out.println("https://jsonplaceholder.typicode.com/posts");
             System.out.println(jsonPlaceHolderClient.getPosts().size());
             System.out.println();
             System.out.println("https://jsonplaceholder.typicode.com/posts/1");
             System.out.println(jsonPlaceHolderClient.getPost(1));
-
         };
     }
 }
